373a9f65159006f0fca44831d1d6df7e2ab3a1c5
Javith Nasim <javith.nasim@mnscorp.net>
2023-08-12 13:10:59 +0530

    first commit created maven project

---
+++ src/main/java/com/maven/leap/App.java
@@ -0,0 +1,13 @@
+package com.maven.leap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
